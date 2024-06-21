12ff36eb20306eca179cc3c5b46d18dcd8b9cbbc
AymanMoustaide <ayman.moustaide@icloud.com>
2024-06-21 04:30:32 +0100

    Random module

--- Random_Module.py
+++ Random_Module.py
@@ -1,4 +1,18 @@
 import random
 
-module_int = random.randint(100, 1000)
-print(module_int)
+
+#Generate random int
+
+random_int = random.randint(100, 1000)
+print(random_int)
+
+#Generate random float only from 0.000 to 0.999
+
+random_float = random.random()
+print(random_float)
+
+
+#Extend a float number to 0.000x to X.xxx
+
+random_float = random.random()
+print(random_float * 100)
