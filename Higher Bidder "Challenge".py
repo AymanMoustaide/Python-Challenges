ee107011eca9249255df88056682c6c2fcd3f2fc
AymanMoustaide <ayman.moustaide@icloud.com>
2024-06-21 03:57:37 +0100

    Random modules

---
+++ Higher Bidder "Challenge".py
@@ -0,0 +1,22 @@
+#Welcome High bidder Game
+Logo = ('''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+
+          Welcome to the secret auction program.
+''')
+
+print (Logo)
+key = str(input('what is your name? :'))
+key = str(input('what is your bid? :'))
+other_bidders = str(input('Are they any other bidder? TYPE "YES" or "NO":'))
+
