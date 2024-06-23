7c1bc688881959e1c50b836167c14f998df1bfb5
AymanMoustaide <ayman.moustaide@icloud.com>
2024-06-23 12:04:12 +0100

    "FOR" loop

---
+++ Loops/For_loops.py
@@ -0,0 +1,83 @@
+
+#_ FOR LOOP 
+
+
+# cars = ['Audi', 'Rolls Royce', 'Nissan GTR']
+
+# #! assinge a variable to each item in cars
+# for a in cars:
+#     print(a)
+
+#_ CODE EXERCISE
+#*WRITE A PROGRAM THAT CALCULATES THE AVERAGE STUDENT HEIGHT FROM A LIST OF HEIGHTS.
+
+# 156 178 165 171 187
+# #!DON'T CHANGE THE CODE BELOW
+
+# student_heights = input("Input a list of student heights ").split()
+# for n in range(0, len(student_heights)):
+#     student_heights[n] = int(student_heights[n])
+
+# # print(student_heights)
+
+
+# #!DON'T CHANGE THE CODE ABOVE 
+
+# total_height = 0 #! EMPTY VARIABLE THAT WILL UPDATE LATER
+# for height in student_heights:
+#     total_height += height
+# print(total_height)
+    
+# total_student = 0 #! EMPTY VARIABLE THAT WILL UPDATE LATER
+# for student in student_heights:
+#     total_student += 1
+# print(total_student)
+
+# average_height = total_height / total_student
+# round_average = round(average_height) #! TO DISPLAY NUMBERS WITHOUT NUMBERS AFTER COMA
+
+# print(f'There are a total of {total_student} student and the average is {round_average}')
+
+# #!WRITE YOUR CODE BELOW THIS ROW 
+
+
+
+#_ CODE EXERCISE TWO (HOW MUCH THIS COMPANY WORTH)
+
+available_cars = ['AUDI', 'NISSAN', 'ROLLS ROYCE', 'MERCEDES','FERRARI']
+each_car_price = ['100000', '80000', '250000', '90000', '120000']
+
+cars_price = [available_cars, each_car_price]
+
+mapping = dict(zip(available_cars, each_car_price)) #! Assinge values in one list to another list using (dict(zip))
+
+mapping_list = list(mapping.items()) #Convert the dictionary to a list of tuples
+
+# print(mapping_list[0])
+
+# for av in available_cars:
+#     print(av)
+
+
+#_ bc = buyer choicen car
+
+
+buyer = input('WHAT IS YOUR DREAM CAR:\n')
+for bc in mapping:
+    if bc == buyer:
+
+        print(f"The price of your dream car, {buyer}, is ${mapping[buyer]}.")
+
+
+        
+    
+
+
+
+
+
+
+
+
+# for ecp in range(0, len(each_car_price)):
+#     print(each_car_price[ecp])
